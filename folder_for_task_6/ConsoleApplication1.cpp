446d23af903a285ea454f7f4269d35dd14341ad0
talantishee <159449156+talantishee@users.noreply.github.com>
2025-07-29 20:10:08 +0700

    Add files via upload

---
+++ folder_for_task_6/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+
+
+using namespace std;
+
+
+
+int main()
+{
+	string line;
+	double mass[100];
+	ifstream in("input.txt");
+	ofstream out;
+	out.open("tsk_6.txt");
+	for (int i = 0; i < 100; i++) {
+		getline(in, line);
+		out << fixed << setprecision(2) << stod(line) << ';' << stod(line) * 0.18 << endl;
+	}
+	out.close();
+	in.close();
+	
+}
