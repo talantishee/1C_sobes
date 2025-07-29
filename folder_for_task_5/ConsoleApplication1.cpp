e24250dfb48f22179be0372c2d8a0d2b42b1ffe6
talantishee <159449156+talantishee@users.noreply.github.com>
2025-07-29 19:38:05 +0700

    Add files via upload

---
+++ folder_for_task_5/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+
+
+using namespace std;
+
+
+
+int main()
+{
+	ofstream out;
+	out.open("tsk_5");
+	srand(time(0));
+
+	for (int i = 0; i < 100; i++) {
+		double x = rand() / 3.0;
+		out << fixed << setprecision(2)<< x << endl;
+	}
+
+	out.close();
+}
